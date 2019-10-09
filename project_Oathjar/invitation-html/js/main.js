0535e2a5a67002a469e07473bc64cbb8fd7f96f5
VitaliR <vitalR@gmail.com>
2019-10-08 19:51:25 -0500

    portfolio

---
+++ project_Oathjar/invitation-html/js/main.js
@@ -0,0 +1,40 @@
+$(document).ready(function(){
+
+$('.about__toggle').click(function(event){
+
+    event.preventDefault();
+    console.log('Click!');
+
+
+    $('.about-content__box').hide();
+
+    var href= $(this).attr('href');
+    console.log(href);
+
+    $(href).fadeIn();
+});
+//следим за скролом и показываем /скрываем кнопку
+$(window).scroll(function(){
+
+    console.log($(this).scrollTop());
+   
+    if($(this).scrollTop()>1280){
+        $('#scrollToTop').fadeIn();
+    }else{
+        $('#scrollToTop').fadeOut();
+    }
+});
+
+
+// При клике на кнопку -делаем прокрутку на верх страницы
+
+$('#scrollToTop').click(function(event){
+    
+    event.preventDefault();
+    $('html').animate({scrollTop: 0}, 800)
+
+});
+
+
+
+});
